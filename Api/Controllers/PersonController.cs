1f6bb030f787d83cf04e069b82a3be81d551a0d4
Sabina Kováříková <sabina.kovarikova@zkl.cz>
2021-12-15 10:37:18 +0100

    Add project files.

---
+++ Api/Controllers/PersonController.cs
@@ -0,0 +1,19 @@
+﻿using Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        [HttpGet]
+        public List<PersonModel> Get()
+        {
+           List<PersonModel> people = new List<PersonModel>();
+                people.Add(new PersonModel {FirstName = "Sabina", LastName = "Kovarikova", Age = 25, IsAlive = true });
+                people.Add(new PersonModel {FirstName = "Miroslav", LastName = "Vaculka", Age = 30, IsAlive = true });
+            return people;
+        }
+    }
+}
---
+++ Api/Models/PersonModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Api.Models
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public bool IsAlive { get; set; } = true;
+    }
+}
